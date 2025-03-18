74e33286e6cb0ba334e697e506fd73800335f498
Jamie Lloyd <jamielloyd.work@gmail.com>
2025-03-18 12:58:59 +0100

    Completed exercise 02

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+   if (num < 0) {
+      return "ERROR"
+   } else {
+      let phrase = ""
+      for (let i = 0; i < num; i++) {
+         phrase += string;
+      }
+      return phrase;
+   }
 };
 
 // Do not edit below this line
