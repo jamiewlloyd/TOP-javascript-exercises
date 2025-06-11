12b666116a33f2c557c81f23abef8b219126c349
Jamie Lloyd <jamielloyd.work@gmail.com>
2025-06-11 11:47:43 +0200

    completed exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+   let i;
+   let number = Number(num);
+   let fib = [0, 1];
 
+   if (number < 0) {
+      return 'OOPS'
+   } else {
+      for (i = 2; i <= number; i++) {
+         fib[i] = fib[i - 2] + fib[i - 1];
+      }
+      return fib[number];
+   }
 };
 
+fibonacci(-10);
 // Do not edit below this line
 module.exports = fibonacci;
